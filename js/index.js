f99ae1c4d5849b3552cd0634bc7d904a926c85de
Purnadip Manna <purnadipmanna2702@gmail.com>
2024-05-20 21:10:50 +0530

    Contact Backend API updated

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-$("#submitbtn").click(function(){
-	let name = document.getElementById('inputName').value;
-		let email = document.getElementById('inputEmail').value;
-		let sub = document.getElementById('inputSubject').value;
-		let msg = document.getElementById('inputMessage').value;
-	$.ajax({type: "POST", url: "https://purnadip-manna.purnadip2702.repl.co/submit", data: {"name" : name, "email" : email, "subject" : sub, "message" : msg}, success: function(result){
-		$("#resText").html(result);
-	}});
+$("#submitbtn").click(function () {
+  let name = document.getElementById("inputName").value;
+  let email = document.getElementById("inputEmail").value;
+  let sub = document.getElementById("inputSubject").value;
+  let msg = document.getElementById("inputMessage").value;
+  $.ajax({
+    type: "POST",
+    url: "https://purnadip2702.pythonanywhere.com/submit",
+    data: { name: name, email: email, subject: sub, message: msg },
+    success: function (result) {
+      $("#resText").html(result);
+    },
+  });
 });
